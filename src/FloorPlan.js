24bbc9b61351a374bcb99cb2f31f52b03823ee37
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-03-26 08:03:09 +0000

    Imports For Interactive Images And Floor4PageAdded

--- src/FloorPlan.js
+++ src/FloorPlan.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo_qa from "./logo_qa.png";
+import ImageMapper from 'react-image-mapper';
 
 
 class App extends Component {
@@ -8,6 +10,7 @@ class App extends Component {
     super(props);
     this.state = {
       floornumber: ' 4',
+      name:"my-map",
     };
   }
 
@@ -16,11 +19,28 @@ class App extends Component {
       <div className="App">
         <br/><br/>
         <h2>Welcome</h2>
-
         <h3>
           This Is The Page For Floor 
           {this.state.floornumber}
           </h3>
+
+<div className="image">
+    <ImageMapper src={logo_qa} width={150} imgWidth={150}
+    	onClick={area => this.clicked(area)}
+    	onMouseEnter={area => this.enterArea(area)}
+    	onMouseLeave={area => this.leaveArea(area)}
+    	onMouseMove={(area, _, evt) => this.moveOnArea(area, evt)}
+    	onImageClick={evt => this.clickedOutside(evt)}
+    	onImageMouseMove={evt => this.moveOnImage(evt)}
+    />
+    {
+    	this.state.hoveredArea &&
+    	<span className="tooltip"
+    	    style={{ ...this.getTipPosition(this.state.hoveredArea)}}>
+    		{ this.state.hoveredArea && this.state.hoveredArea.name}
+    	</span>
+    }
+</div>
 
         <br /><br />
       </div>
